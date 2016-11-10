644703d6592fa8eb4ae58e68be584fbc472667e8
Marcelo Muniz <marcelo.pjmuniz@gmail.com>
2016-11-10 21:36:32 -0200

    Reorganização da estrutura dos arquivos, adição da execução via Bash

--- src/main.py
+++ src/main.py
@@ -8,33 +8,45 @@ import math
 #   seed: semente para geração dos números
 #   size: tamanho da lista
 #   ub: upper bound, limite superior para geração dos números
+#
+#   retorna: lista gerada
 def getRandomList(seed, size, ub):
     random.seed(seed)
     # lista de tamanho size
     rdList = []
-    for slot in rdList:
+    for i in range(1, size):
         # random.uniform: retorna um número aleatório entre a e b
-        rdList.append(random.uniform(a = 1, b = ub))
+        rdList.append(int(random.uniform(a = 1, b = ub)))
 
     return rdList
 
 #   Função responsável por verificar se um dado número num é primo ou não
 #   num: número à ser verificado
 def isPrime(num):
-    # método ineficiente para verificação
-    return num > 1 and all(num % i for i in range(2, int(math.sqrt(num))))
+    # método ineficiente para verificação, mas rápido de implementar
+    return num > 1 and all(num % i for i in range(2, int(num)))
 
 #   Função responsável por escrever lista no arquivo, no formato correto
+#   randomList: lista à ser escrita
+#   filePath: arquivo à receber lista
 def writeList(randomList, filePath):
-    fileRef = open(filePath, 'w+')
-    fileRef.write("%d\n" % len(randomList))
+    #with fecha arquivo sozinho, propriamente
+    with open(filePath, 'w+') as fileRef:
+        fileRef.write("%d\n" % len(randomList))
 
-    for num in randomList:
-        fileRef.write("%d " % num)
+        for num in randomList:
+            fileRef.write("%d " % num)
 
 #   Função responsável por ler lista do arquivo, no formato correto
-#   TODO: ler input formatado
-def readList(fileRef):
-    fileRef = open(filePath, 'r+')
-    content = fileRef.read()
+#   filePath: caminho do arquivo à ser lido
+#   retorna: lista com números do arquivo
+def readList(filePath):
+    with open(filePath, 'r+') as fileRef:
+        size = int(fileRef.readline())
+        numbers = fileRef.readline().split(' ')
+        numbers.pop()
+        randomNumbers = []
+        for i in numbers:
+            randomNumbers.append(int(i))
+        return randomNumbers
 
