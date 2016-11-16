e95156781a3cd4c3f4aeb47c01aea9501dd1fa53
vitornl <vitornaslourenco@gmail.com>
2016-11-16 11:02:09 -0200

    Seq.py modificado para mapping e reduce e funcionando

--- src/seq.py
+++ src/seq.py
@@ -9,23 +9,22 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
-recv_buffer = numpy.array([-1,-1])
-rd_list = ru.getRandomList(sys.argv[1], sys.argv[2], sys.argv[3])
-prime = []
-for i in rd_list:
-    print(i)
-    
-if rank == 0:
-    for i in range(1, size):
-        comm.Recv(recv_buffer, source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG)
-        print("G:" + str(recv_buffer[0]) + ":" + str(recv_buffer[1]))
-        if(recv_buffer[0]): #Se número é primo ou não
-            prime.append(recv_buffer[1]) #NÚMERO PRIMO            
-else:
-    n = rd_list.pop() #PROBLEMA AQUI ELE NÃO DÁ POP CERTO, PRECISA FAZER O ACESSO CONCORRENTE A ESSA LISTA!!!
-    n_np = ru.isPrime(n)
-    print("R:" + str(rank) + "|" + str(n) + "|" + str(n_np[0]) + ":" + str(n_np[1]))
-    comm.Send(n_np, dest=0, tag=rank)
+n_prime = -1
 
-if comm.rank == 0:
-    ru.writeList(prime, "primos_seq")
+if(rank == 0):
+    rd_list = ru.getRandomList(sys.argv[1], sys.argv[2], sys.argv[3])
+else:
+    rd_list = None
+
+rd_list = comm.scatter(rd_list, root = 0)
+n_np = ru.isPrime(rd_list)
+#print("R:" + str(rank) + "|" + str(n_np[0]) + ":" + str(n_np[1]))
+if(n_np[0] == 1):
+    n_prime = n_np[1]
+    
+comm.Barrier()
+n_prime = comm.gather(n_prime, root=0)
+if(rank == 0):
+    while -1 in n_prime:
+        n_prime.remove(-1)
+    ru.writeList(n_prime, "primos_seq")
--- src/test.py
+++
@@ -1,5 +0,0 @@
-import numpy
-
-rs = numpy.zeros(1).append(2)
-
-print(rs)
